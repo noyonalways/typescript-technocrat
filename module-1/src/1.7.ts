46dc4fee7b7703fac5d28d56ea50f4cb57743874
noyonalways <noyonrahman2003@gmail.com>
2024-04-30 23:02:57 +0600

    complete spread and rest operator

---
+++ module-1/src/1.7.ts
@@ -0,0 +1,44 @@
+//  Spread and Rest Operator
+
+// spread operator
+const bros1: string[] = ["nadim", "shakil", "asif"];
+const bros2: string[] = ["mahfuj", "asraf", "sabit"];
+
+bros1.push(...bros2);
+
+// const allBros: string[] = [...bros, ...bros2];
+
+const mentors1 = {
+  typescript: "Mezba",
+  redux: "Mir",
+  dbms: "Mizan",
+};
+
+const mentors2 = {
+  prisma: "Firoz",
+  next: "Tanmoy",
+  cloud: "Nahid",
+};
+
+const allMentors = {
+  ...mentors1,
+  mentors2,
+};
+
+// rest operator
+
+// without rest operator
+const greetFriends1 = (friend1: string, friend2: string, friend3: string) => {
+  console.log(`Hi ${friend1}`);
+  console.log(`Hi ${friend2}`);
+  console.log(`Hi ${friend3}`);
+};
+
+greetFriends1("Nadim", "Shakil", "Asif");
+
+// with rest operator
+const greetFriends2 = (...friends: string[]) => {
+  friends.forEach((friend) => {
+    console.log(`Hi ${friend}`);
+  });
+};
