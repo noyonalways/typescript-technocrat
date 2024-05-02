e043a05d95b4f49b1393dfb3a969f8bbb9a2295d
noyonalways <noyonrahman2003@gmail.com>
2024-05-02 19:51:46 +0600

    lecture-1: type asssertion complete

--- module-2/src/2.1.ts
+++ module-2/src/2.1.ts
@@ -10,11 +10,32 @@ anything = 222;
 (anything as number).toFixed(2); // anything as number
 console.log(anything);
 
-const kilogramToGrams = (value: string | number) => {
+const kilogramToGrams = (
+  value: string | number
+): string | number | undefined => {
   if (typeof value === "string") {
-    const convertedValue = parseFloat(value);
+    const convertedValue = parseFloat(value) * 1000;
     return `The converted value is: ${convertedValue}`;
   } else if (typeof value === "number") {
     return value * 1000;
   }
 };
+
+const result1 = kilogramToGrams(100) as number;
+const result2 = kilogramToGrams("100") as string;
+
+// we need to be very careful when we use type assertion
+
+console.log(result1);
+console.log(result2);
+
+// type assertion in customError in try catch block
+type CustomError = {
+  message: string;
+};
+
+try {
+  // do something...
+} catch (err) {
+  console.log((err as CustomError).message);
+}
