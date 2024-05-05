4d11998a206466912d676241d0b4599eaad5be32
noyonalways <noyonrahman2003@gmail.com>
2024-05-05 18:24:31 +0600

    lec-1: Object and Class completed

--- module-3/src/3.1.ts
+++ module-3/src/3.1.ts
@@ -3,13 +3,28 @@
 // oop - class
 
 class Animal {
-  name: string;
-  species: string;
-  sound: string;
+  // public name: string;
+  // public species: string;
+  // public sound: string;
 
-  constructor(name: string, species: string, sound: string) {
-    this.name = name;
-    this.species = species;
-    this.sound = sound;
+  // using parameter properties
+
+  constructor(
+    public name: string,
+    public species: string,
+    public sound: string
+  ) {
+    // this.name = name;
+    // this.species = species;
+    // this.sound = sound;
+  }
+
+  makeSound() {
+    console.log(`The ${this.name} sound is ${this.sound}`);
   }
 }
+
+const cat = new Animal("Tom", "cat", "Meow Meow");
+const dog = new Animal("Husky", "cat", "Gheow Ghew");
+
+cat.makeSound();
